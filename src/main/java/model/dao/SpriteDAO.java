d3b480ab0bdf130c3bd159932424f81705c7f402
lebonhomme17 <antoinelepetitjean@gmail.com>
2019-11-28 12:41:29 +0100

    affichage des pv

--- src/main/java/model/dao/SpriteDAO.java
+++ src/main/java/model/dao/SpriteDAO.java
@@ -4,6 +4,7 @@ import model.SpriteFactory;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 
@@ -26,6 +27,7 @@ public class SpriteDAO implements ISpriteDAO {
         BufferedImage trap = null;
         BufferedImage hero = null;
         BufferedImage monster = null;
+        BufferedImage life = null;
         try {
             grass = ImageIO.read(new File("assets/grass32x32.png"));
             wall = ImageIO.read(new File("assets/wall32x32.png"));
@@ -33,10 +35,11 @@ public class SpriteDAO implements ISpriteDAO {
             trap = ImageIO.read(new File("assets/trap32x32.png"));
             hero = ImageIO.read(new File("assets/hero32x32.png"));
             monster = ImageIO.read(new File("assets/enemy32x32.png"));
+            life = ImageIO.read(new File("assets/life32x32.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return new SpriteFactory(grass, wall, chest, trap, hero, monster);
+        return new SpriteFactory(grass, wall, chest, trap, hero, monster, life);
     }
 }
